b5de6daa7b65085fc18ffc7d4cdfcfd4bb2ecaeb
streamlesswaveson <streamlesswaveson@gmail.com>
2018-06-29 16:44:17 -0700

    3-23 configuring yargs commands

--- 3-notes-node/app.js
+++ 3-notes-node/app.js
@@ -1,14 +1,37 @@
-
-const fs = require('fs');
 const notes = require('./notes.js');
 const _ = require('lodash');
 const yargs = require('yargs')
 
-var argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+
+var argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', "list all notes")
+    .command('read', 'read a note', {
+        title: titleOptions,
+    })
+    .command('remove', 'remove a note', {
+        title: titleOptions,
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 // console.log(argv)
 
-if (command=== 'add') {
+if (command === 'add') {
     var note = notes.addNote(argv.title, argv.body);
     if (!note) {
         console.log('Unable to add note. Probably duplicate')
