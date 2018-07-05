82ad627a1a8932d5756cb5bf5cbd0f15df8a9c3d
streamlesswaveson <streamlesswaveson@gmail.com>
2018-07-05 06:35:40 -0700

    5-44 - handlebars 2, partials, helpers

--- 5-webserver/server.js
+++ 5-webserver/server.js
@@ -3,22 +3,30 @@ const hbs = require('hbs')
 
 var app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
+
 app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+   return text.toUpperCase()
+})
+
 app.get('/', (req, res) => {
     res.render('welcome.hbs', {
         pageTitle: 'Welcome',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Hello message'
     })
 });
 
 app.get('/about', (req, res) => {
     res.render('about.hbs',{
-        pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
+        pageTitle: 'About Page'
     })
 })
 
