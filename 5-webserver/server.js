8130e15fb5a58a0457e9b3364849901289626187
streamlesswaveson <streamlesswaveson@gmail.com>
2018-07-06 05:34:56 -0700

    5-45 middleware

--- 5-webserver/server.js
+++ 5-webserver/server.js
@@ -1,21 +1,40 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs');
 
-var app = express()
+const app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
 
 app.set('view engine', 'hbs')
 
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server log', err);
+        }
+    });
+    next();
+});
+
+// the relative placement of this is important - they are fired in the order used here.
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
 app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear()
-})
+});
 
 hbs.registerHelper('screamIt', (text) => {
    return text.toUpperCase()
-})
+});
 
 app.get('/', (req, res) => {
     res.render('welcome.hbs', {
