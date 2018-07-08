12883b6eec75733137f8001bcee61c62cf8708ab
streamlesswaveson <streamlesswaveson@gmail.com>
2018-07-08 06:12:35 -0700

    6-56 using describe

--- 6-tests/server/server.test.js
+++ 6-tests/server/server.test.js
@@ -3,39 +3,48 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect(200)
-        .expect('hello world')
-        .end(done)
-});
+describe('Server', () => {
 
-it('should return 404 response', (done)=>{
-    request(app)
-        .get('/404')
-        .expect(404)
-        .expect({
-            error:'page not found'
-        })
-        .end(done)
-});
 
-it('should return custom response', (done)=>{
-    request(app)
-        .get('/custom')
-        .expect((res)=>{
-            expect(res.body).toInclude({name:'foo bar'})
-        })
-        .end(done)
-});
+    describe('GET /', () => {
 
-it('should return your user', (done)=>{
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res)=>{
-            expect(res.body).toInclude({name:'Streamless', age: 42})
-        })
-        .end(done)
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect(200)
+                .expect('hello world')
+                .end(done)
+        });
+    });
+
+    it('should return 404 response', (done) => {
+        request(app)
+            .get('/404')
+            .expect(404)
+            .expect({
+                error: 'page not found'
+            })
+            .end(done)
+    });
+
+    it('should return custom response', (done) => {
+        request(app)
+            .get('/custom')
+            .expect((res) => {
+                expect(res.body).toInclude({name: 'foo bar'})
+            })
+            .end(done)
+    });
+
+    describe('GET /users', () => {
+        it('should return your user', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({name: 'Streamless', age: 42})
+                })
+                .end(done)
+        });
+    });
 });
--- 6-tests/utils/utils.test.js
+++ 6-tests/utils/utils.test.js
@@ -1,60 +1,68 @@
 const utils = require('./utils');
 const expect = require('expect');
 
-it('should add two numbers', () => {
-    var result = utils.add(33, 11);
-    expect(result)
-        .toBe(44)
-        .toBeA('number');
-});
+describe('Utils', () => {
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4,3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
-    })
-});
 
-it('should square two numbers', () => {
-    var result = utils.square(3)
-    expect(result)
-        .toBe(9)
-        .toBeA('number');
-});
+    describe('#add', () => {
 
-it('should async square two numbers', (done) => {
-    utils.asyncSquare(4, (s) => {
-        expect(s).toBe(16).toBeA('number');
-        done();
-    })
-});
+        it('should add two numbers', () => {
+            var result = utils.add(33, 11);
+            expect(result)
+                .toBe(44)
+                .toBeA('number');
+        });
 
-it('should verify first and last names', ()=>{
-    var user = {};
-    var name = 'Andrew Mead';
-    utils.setName(user, name);
-    expect(user).toInclude({
-        firstName: 'Andrew',
-        lastName: 'Mead'
-    })
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            })
+        });
+    });
+
+    it('should square two numbers', () => {
+        var result = utils.square(3)
+        expect(result)
+            .toBe(9)
+            .toBeA('number');
+    });
+
+    it('should async square two numbers', (done) => {
+        utils.asyncSquare(4, (s) => {
+            expect(s).toBe(16).toBeA('number');
+            done();
+        })
+    });
+
+    it('should verify first and last names', () => {
+        var user = {};
+        var name = 'Andrew Mead';
+        utils.setName(user, name);
+        expect(user).toInclude({
+            firstName: 'Andrew',
+            lastName: 'Mead'
+        })
+    });
+
 });
 
 // it('should expect some values', () => {
-    // expect(12).toNotBe(2);
-    // same object assertion
-    // expect({'name':"Andrew"}).toBe({'name':"Andrew"})
+// expect(12).toNotBe(2);
+// same object assertion
+// expect({'name':"Andrew"}).toBe({'name':"Andrew"})
 
-    // looks at object properties
-    // expect({'name':"Andrew"}).toEqual({'name':"Andrew"})
+// looks at object properties
+// expect({'name':"Andrew"}).toEqual({'name':"Andrew"})
 
-    // expect([2,3,4]).toInclude(5)
-    //  expect([2,3,4]).toExclude(5)
+// expect([2,3,4]).toInclude(5)
+//  expect([2,3,4]).toExclude(5)
 
-    // expect({
-    //     name: 'Andrew',
-    //     age: 25,
-    //     location: 'Philadelphia'
-    // }).toInclude({
-    //     age: 25
-    // })
+// expect({
+//     name: 'Andrew',
+//     age: 25,
+//     location: 'Philadelphia'
+// }).toInclude({
+//     age: 25
+// })
 // });
