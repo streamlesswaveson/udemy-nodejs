ed9cdc8d5f071ac4d34859a1367bc750dd1f662b
streamlesswaveson <streamlesswaveson@gmail.com>
2018-07-10 05:00:26 -0700

    7-70 mongoose intro

---
+++ 7-todo-api/server.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
+    }
+});
+
+// var newTodo = new Todo({
+//     text: 'Cook dinner'
+// });
+
+var newTodo = new Todo({
+    text: 'Practice patience',
+    completed:true,
+    completedAt:new Date().getTime()
+});
+
+newTodo.save().then((result)=> {
+    console.log(result);
+}, (err)=> {
+    console.log('failed to save', err);
+});
